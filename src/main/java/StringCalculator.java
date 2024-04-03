2c4aedb72c543aa430c14498b774430f03606932
fblomster <77675106+fblomster@users.noreply.github.com>
2024-04-03 14:20:58 +0300

    First lab done

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String numbers) {
-
         if (numbers.isEmpty()) {
             return 0;
         }
-        String[] nums = numbers.split(",");
+
+        //custom delimiter
+        String delimiter = ",";
+
+        //checkar av vilken delimiter som kommer efter // och omvandlar till custom delimiter och splittar
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterIndex);
+            numbers = numbers.substring(delimiterIndex + 1);
+        }
+
+        String[] nums = numbers.split("[,\n;]");
         int sum = 0;
+        List<String> negativeNumbers = new ArrayList<>();
         for (String num : nums) {
-            sum += Integer.parseInt(num);
+            int n = toInt(num);
+            if (n < 0) {
+                negativeNumbers.add(num);
+            }
+            sum += n;
+        }
+
+        // Throw exception if negative numbers found
+        if (!negativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException("Negatives not allowed: " + String.join(", ", negativeNumbers));
         }
         return sum;
+        }
+
+    private static int toInt(String numbers) throws NumberFormatException {
+        return Integer.parseInt(numbers);
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 public class StringCalculatorTest {
 
     private StringCalculator calculator;
@@ -17,12 +20,32 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void testSingleNumberReturnsNumberItself() {
+    public void testSingleNumberReturnsItself() {
         Assertions.assertEquals(1, calculator.add("1"));
     }
 
     @Test
     public void testTwoNumbersSeparatedByComma() {
         Assertions.assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    public void testMultipleNumbers() {
+        Assertions.assertEquals(10, calculator.add("1,2,2,5"));
+    }
+
+    @Test
+    public void testMultipleNumbersSeparatedByCommaOrNewLine() {
+        Assertions.assertEquals(6, calculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void testCustomDelimiter() {
+        Assertions.assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testExceptionOnNegatives() {
+            calculator.add("-1,-2,-3");
     }
 }
