ef4b78c455df9142ecc6f314555dea9622b33a43
frida <frida.blomster@ha.ax>
2024-04-22 13:44:31 +0300

    Log above 1000

--- src/test/java/StringCalculatorLoggingTests.java
+++ src/test/java/StringCalculatorLoggingTests.java
@@ -1,24 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class StringCalculatorLoggingTests {
 
+    private Logger mockLogger;
+    private StringCalculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        mockLogger = mock(Logger.class);
+        calculator = new StringCalculator(mockLogger);
+    }
+
     @Test
     public void testStringCalculatorDoesNotLogNumbersBelow1000(){
-        Logger mockLogger = mock(Logger.class);
-        StringCalculator calculator = new StringCalculator(mockLogger);
-
         calculator.add("");
-        verify(mockLogger, times(0)).log(anyInt());
+        verify(mockLogger, never()).log(anyInt());
     }
 
     @Test
     public void testLoggerLogsNumbersAbove1000(){
-        Logger mockLogger = mock(Logger.class);
-        StringCalculator calculator = new StringCalculator(mockLogger);
-
         calculator.add("1001");
-
         verify(mockLogger, times(1)).log(1001);
     }
 }
